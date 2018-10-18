755f6a5af5aea78591af72a850f60b7b991edeed
My Name <harold.hu@zoom.us>
2018-10-18 17:47:49 +0800

    Add digit

--- src/test/java/org/freeswitch/esl/client/ClientExample.java
+++ src/test/java/org/freeswitch/esl/client/ClientExample.java
@@ -1,15 +1,17 @@
 package org.freeswitch.esl.client;
 
 import java.net.InetSocketAddress;
-import java.util.Iterator;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.freeswitch.esl.client.inbound.Client;
 import org.freeswitch.esl.client.internal.IModEslApi.EventFormat;
+import org.freeswitch.esl.client.transport.CommandResponse;
 import org.freeswitch.esl.client.transport.SendMsg;
 import org.freeswitch.esl.client.transport.event.EslEvent;
 import org.slf4j.Logger;
@@ -20,13 +22,18 @@ import com.google.common.base.Throwables;
 public class ClientExample {
     private static final Logger L = LoggerFactory.getLogger(ClientExample.class);
     
-//    private static String host = "10.100.57.78";
-    private static String host = "192.168.1.104";
+    private static String host = "10.100.57.78";
+//    private static String host = "192.168.1.104";
     private static int port = 8021;
     private static String password = "ClueCon"; 
-    private static LinkedBlockingQueue<String> toDoCmdQueue = new LinkedBlockingQueue<String>();
-    private static ConcurrentHashMap<String,CompletableFuture<EslEvent>> ayncEventResult = new ConcurrentHashMap<String,CompletableFuture<EslEvent>>();
+    private static String path = "/usr/share/freeswitch/sounds/en/us/callie/ivr/8000/";
+    static String prompt = path + "ivr-please_enter_extension_followed_by_pound.wav";
+    static String failed = path + "ivr-that_was_an_invalid_entry.wav";
     
+    
+    private static LinkedBlockingQueue<Object> toDoCmdQueue = new LinkedBlockingQueue<>();
+    private static ConcurrentHashMap<Object,CompletableFuture<EslEvent>> ayncEventResult = new ConcurrentHashMap<>();
+    private static AtomicInteger msgID = new AtomicInteger(0);
     private static Client client = new Client();
    /*
     sendmsg 8cf3f692-cedd-4470-98b8-5cc8225d22f7
@@ -39,6 +46,8 @@ public class ClientExample {
     nomedia-uuid: mute_test
     
     conference list
+    
+    event 
     
     conference freeswitch play /usr/share/freeswitch/sounds/music/8000/suite-espanola-op-47-leyenda.wav
     conference 3000-192.168.1.104 stop all
@@ -81,41 +90,54 @@ pause 7285cf3e-7fe7-4629-927b-b637213e9e83 on
             client.addEventListener(
             		(ctx, event) ->{
             			L.info("Received event - name: {}", event.getEventName());
-            			L.info("Received event - event header:  {}", event.getEventHeaders().toString());
+//            			L.info("Received event - event header:  {}", event.getEventHeaders().toString());
 //            			L.info("Received event - event body:  {}", event.getEventBodyLines().toString());
             			
-//            			Map<String, String> vars = event.getEventHeaders();
+            			Map<String, String> vars = event.getEventHeaders();
 //            			String uuid = vars.get("unique-id");
 //            			L.info("Received event - unique-id:  {}", uuid);
-//                        if (event.getEventName().equals("CHANNEL_EXECUTE_COMPLETE") 
-//                        		&& vars.get("Application").equals("conference")) {
-//                        	L.info("Received event - CHANNEL_EXECUTE_COMPLETE:  {}", event.getEventBodyLines().toString());
-//                        }
+                        if (event.getEventName().equals("DTMF") 
+//                        		&& vars.get("Application").equals("conference")
+                        		) {
+                        	L.info("Received event - event DTMF-Digit:  {}", event.getEventHeaders().get("DTMF-Digit"));
+//                        	L.info("Received event - event body:  {}", event.getEventBodyLines().toString());
+                        }
                         
             		}
             );
 
             client.connect(new InetSocketAddress(host, port), password, 10);
 //            client.setEventSubscriptions(EventFormat.PLAIN, "CHANNEL_BRIDGE|CHANNEL_STATE|CALL_UPDATE");
-            client.setEventSubscriptions(EventFormat.PLAIN, "BACKGROUND_JOB");
+//            client.setEventSubscriptions(EventFormat.PLAIN, "BACKGROUND_JOB|CHANNEL_EXECUTE");
+            client.setEventSubscriptions(EventFormat.PLAIN, "ALL");
+
 //            client.setEventSubscriptions(EventFormat.PLAIN, "all");
             
             new Thread(()-> {
             	L.info("excute task thread start.");
             	while(true) {
-            		String cmd;
+            		Object cmdObj;
 					try {
-						cmd = toDoCmdQueue.take();
-						L.info("====>>send cmd: {}", cmd);
-						String[] cmdArr = cmd.split(",");
-	            		CompletableFuture<EslEvent> eslEvent = client.sendBackgroundApiCommand(cmdArr[0], cmdArr[1]);
+						cmdObj = toDoCmdQueue.take();
+						if(cmdObj instanceof String) {
+							String cmd = (String) cmdObj;
+							L.info("====>>send cmd: {}", cmd);
+							String[] cmdArr = cmd.split(",");
+		            		CompletableFuture<EslEvent> eslEvent = client.sendBackgroundApiCommand(cmdArr[0], cmdArr[1]);
+		            		ayncEventResult.put(cmdObj, eslEvent);
+	//	            		confMap.put(eslEvent.get().getEventBodyLines().toString(), cmd);
+	//	       	          	L.info("Async uuid:{}", eslEvent.get().getEventBodyLines().toString());
 	            		
-//	            		confMap.put(eslEvent.get().getEventBodyLines().toString(), cmd);
-//	       	          	L.info("Async uuid:{}", eslEvent.get().getEventBodyLines().toString());
 	            		
-	            		ayncEventResult.put(cmd, eslEvent);
-	            		
-	            		
+						}else if(cmdObj instanceof SendMsg) {
+							SendMsg tmp = (SendMsg) cmdObj;
+							L.info("====>>send cmd: {}", tmp.getMsgLines().toString());
+							CommandResponse rsp = client.sendMessage(tmp);
+							L.info("<<====recv cmd: {}", rsp.getReplyText().toString());
+//							ayncEventResult.put(cmdObj, eslEvent);
+						}
+						
+						
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						L.error("InterruptedException:",e);
@@ -131,29 +153,34 @@ pause 7285cf3e-7fe7-4629-927b-b637213e9e83 on
             	L.info("get aync task result thread start.");
             	while(true) {
 					try {
-						for(Entry<String, CompletableFuture<EslEvent>> iter: ayncEventResult.entrySet()) {
+						for(Entry<Object, CompletableFuture<EslEvent>> iter: ayncEventResult.entrySet()) {
 							try {
-								String cmd = iter.getKey();
-								
-								String[] cmdArr = cmd.split(",");
-								EslEvent eslEvent = iter.getValue().get();
-								if(null != eslEvent) {
-									L.info("<<==== recv msg: {}", eslEvent.getEventHeaders().toString());
-									L.info("<<==== recv msg body: {}", eslEvent.getEventBodyLines().toString());
-									String msgBody = eslEvent.getEventBodyLines().toString();
-									if(msgBody.contains("+OK")) {
-										String msgBodyArr[] = msgBody.split("\\s+");
-										if( cmd.contains("user/1000") && msgBodyArr.length==2 ) {
-											String uuid = msgBodyArr[1].substring(0,msgBodyArr[1].length()-1);
-											Thread.sleep(20*1000);
-//											toDoCmdQueue.add("uuid_broadcast,".concat(uuid).concat(" /usr/share/freeswitch/sounds/en/us/callie/conference/8000/conf-enter_conf_pin.wav"));
-											toDoCmdQueue.add("uuid_broadcast,".concat(uuid).concat(" /usr/share/freeswitch/sounds/music/8000/suite-espanola-op-47-leyenda.wav"));
-											Thread.sleep(2*1000);
-											toDoCmdQueue.add("pause,".concat(uuid).concat(" on"));
+								if(iter.getKey() instanceof String) {
+									String cmd = (String)iter.getKey();
+									String[] cmdArr = cmd.split(",");
+									EslEvent eslEvent = iter.getValue().get();
+									if(null != eslEvent) {
+										L.info("<<==== recv msg: {}", eslEvent.getEventHeaders().toString());
+										L.info("<<==== recv msg body: {}", eslEvent.getEventBodyLines().toString());
+										String msgBody = eslEvent.getEventBodyLines().toString();
+										if(msgBody.contains("+OK")) {
+											String msgBodyArr[] = msgBody.split("\\s+");
+											if( cmd.contains("user/1000") && msgBodyArr.length==2 ) {
+												String uuid = msgBodyArr[1].substring(0,msgBodyArr[1].length()-1);
+//												Thread.sleep(30*1000);
+//												toDoCmdQueue.add("uuid_broadcast,".concat(uuid).concat(" /usr/share/freeswitch/sounds/en/us/callie/conference/8000/conf-enter_conf_pin.wav"));
+//												toDoCmdQueue.add("uuid_broadcast,".concat(uuid).concat(" /usr/share/freeswitch/sounds/music/8000/suite-espanola-op-47-leyenda.wav"));
+//												Thread.sleep(10*1000);
+//												toDoCmdQueue.add("pause,".concat(uuid).concat(" on"));
+												Thread.sleep(10*1000);
+												String cmdArgs = "0 20 6 10000 # ".concat(prompt).concat(" ").concat(failed).concat(" dtmf \\\\d+ 10000");
+												SendMsg msg = getSendMsg(uuid, "execute", "play_and_get_digits", cmdArgs);
+												toDoCmdQueue.add(msg);
+											}
 										}
 									}
+									ayncEventResult.remove(cmd);  // it's ok in concurrentHashMap
 								}
-								ayncEventResult.remove(cmd);  // it's ok in concurrentHashMap
 							} catch (ExecutionException e) {
 								// TODO Auto-generated catch block
 								L.error("ExecutionException:",e);
@@ -190,7 +217,7 @@ pause 7285cf3e-7fe7-4629-927b-b637213e9e83 on
             
 //            SendMsg msg = getSendMsg("48f3b785-08fb-4174-9f5a-e814e1ec44f1", "execute", "playback", "/usr/share/freeswitch/sounds/music/8000/suite-espanola-op-47-leyenda.wav");
 //            client.sendMessage(msg);
-            
+              
         } catch (Throwable t) {
             Throwables.propagate(t);
         }
